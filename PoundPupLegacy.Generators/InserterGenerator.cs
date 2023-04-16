fb536ea9f8c7c3847f0fa7114b4004ad40e0f5da
Niels Hoogeveen <pd_aficionado@hotmail.com>
2023-04-17 01:59:05 +0200

    Added generator project, no implementation yet.

---
+++ PoundPupLegacy.Generators/InserterGenerator.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.CodeAnalysis;
+
+namespace PoundPupLegacy.Meta;
+
+[Generator]
+public class InserterGenerator : IIncrementalGenerator
+{
+    public void Initialize(IncrementalGeneratorInitializationContext context)
+    {
+    }
+}
+
